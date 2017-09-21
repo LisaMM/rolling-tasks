d880fd8461bb32536c03d58875e05f96dcdff747
lsteendam <lst@foreach.be>
2017-09-21 14:09:08 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Intro } from './components/Intro';
+import logo from './logo.svg';
+import './App.css';
+
+function fetchHabiticaTasks() {
+    const headers = {
+        'x-api-key': '9ca67b5a-4f72-4200-81c8-345c534e6234',
+        'x-api-user': 'c4484f97-e965-4748-8a09-467270680338'
+    };
+    console.log(headers);
+    return fetch('https://habitica.com/api/v3/tasks/user', { headers: headers })
+}
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSize: 0
+        }
+    };
+
+    componentWillMount() {
+        fetchHabiticaTasks()
+            .then(
+                result => result.json()
+            )
+            .then(
+                (res) => {
+                    console.log(res);
+                    this.setState({
+                        dataSize: res.data.length
+                    });
+                }
+            )
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React, Lisa!</h2>
+                </div>
+                <p className="App-intro">
+                    Size of my data is {this.state.dataSize};
+                </p>
+                <Intro name="Zsa-Zsa" secondName="Tiba" />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Intro extends Component {
+    propTypes = {
+        name: PropTypes.string,
+        secondName: PropTypes.string
+    };
+
+    render() {
+        console.log(this.props);
+        const { name, secondName } = this.props;
+        return (
+            <div>
+                {name} en {secondName} zijn mooi.
+            </div>
+        )
+    }
+}
