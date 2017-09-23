b89d24e28d07fe3b35e36ededeb5e75f768e95ed
lsteendam <lst@foreach.be>
2017-09-23 17:42:22 +0200

    Christophe is een held

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 
 export default class Intro extends Component {
     propTypes = {
-        name: PropTypes.string,
-        secondName: PropTypes.string
+      name: PropTypes.string,
+      secondName: PropTypes.string,
     };
 
     render() {
-        console.log(this.props);
-        const { name, secondName } = this.props;
-        return (
-            <div>
-                {name} en {secondName} zijn mooi.
-            </div>
-        )
+      console.log(this.props);
+      const { name, secondName } = this.props;
+      return (
+        <div>
+          {name} en {secondName} zijn mooi.
+        </div>
+      );
     }
 }
